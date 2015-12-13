ab3c810b7ca5d26d11ae66bcd06393f08d28153f
theo tran <theotran@rocketmail.com>
2015-12-12 15:27:09 -1000

    got some functionality in the start/stop methods for timer

--- Delta-Force.js
+++ Delta-Force.js
@@ -1,14 +1,12 @@
 var EventEmitter = require('events');
 
+
 //module.exports = Timer;
 
 function Timer () {
   EventEmitter.call(this);
-
-  var self = this;
-  setInterval(function () {
-    self.emit('tick');
-    }, 1000);
+  var intervalId = null;
+  
 }
 
 Timer.prototype = new Object(EventEmitter.prototype, {
@@ -17,10 +15,52 @@ Timer.prototype = new Object(EventEmitter.prototype, {
   }
 });
 
+Timer.prototype.start = function (tickCount) {
+  var counter = 0;
+  //self reps the instance of timer
+  var self = this;
+  tickCount = tickCount || 60;
+  var startTime = Date.now();
+
+  //emitting something called 'start', if youre going to emit something you have to have a listener
+  self.emit('start', {startTime: startTime});
+  this.intervalId = setInterval(function () {
+    counter++;
+    if (counter > tickCount) {
+      self.stop();
+    } else {
+      self.emit('tick', {counter: counter});
+    }
+  }, 1000);
+};
+
+Timer.prototype.stop = function () {
+
+  var self = this;
+  var endTime = Date.now();
+  self.emit('stop', {endTime: endTime});
+  
+  clearInterval(this.intervalId);
+
+};
+
 var basicTimer = new Timer();
-basicTimer.addListener('tick', function (event) {
-  process.stdout.write('tick \n');
-});
+
+
+
+function tickHandler (event) {
+  console.log(event);
+}
+
+basicTimer.addListener('tick', tickHandler);
+
+basicTimer.addListener('start', tickHandler);
+
+basicTimer.addListener('stop', tickHandler);
+
+basicTimer.start(10);
+
+
 
 
 
